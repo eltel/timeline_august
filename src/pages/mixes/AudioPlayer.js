22b0c1f69336dc02b8b1d3f9aacb9158feb832ea
Terry Mitchell <djterrymitchell@gmail.com>
2017-08-28 12:21:18 -0300

    CSS and menu changes

--- src/pages/mixes/AudioPlayer.js
+++ src/pages/mixes/AudioPlayer.js
@@ -4,6 +4,8 @@ import './AudioPlayer.css';
 
 require('wavesurfer.js');
 
+var vars = {clip_id:"2dduo643aqtc",transparent:"false",pause:"0",repeat:"1",bg_color:"",fs_mode:"2",no_controls:"0",start_img:"0",start_volume:"100",close_button:"",brand_new_window:"1",auto_hide:"1",stretch_video:"1",player_align:"NONE",offset_x:"",offset_y:"",player_color_ratio:0.6,skinAlpha:"50",colorBase:"#58630e",colorIcon:"#FFFFFF",colorHighlight:"#778f1f",direct:"true",is_responsive:"true",viewers_limit:0,cc_position:"bottom",cc_positionOffset:70,cc_multiplier:0.03,cc_textColor:"#ffffff",cc_textOutlineColor:"#ffffff",cc_bkgColor:"#000000",cc_bkgAlpha:0.7};
+
 
 class AudioPlayer extends Component {
 	constructor(props) {
