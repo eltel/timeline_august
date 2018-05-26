f7b5ea8deeb233a932b6a1e35054b54ade82d9f5
Terry Mitchell <djterrymitchell@gmail.com>
2018-05-26 12:42:35 -0300

    v7
    v7

--- src/pages/mixes/AudioPlayer.js
+++ src/pages/mixes/AudioPlayer.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 // import Wavesurfer from 'react-wavesurfer';
-import './AudioPlayer.css';
+ import './AudioPlayer.css';
 // import {Button, IconButton} from 'react-toolbox/lib/button';
 // import Button from 'react-bootstrap/lib/Button';
-// import ReactAudioPlayer from 'react-audio-player';
+//  import ReactAudioPlayer from 'react-audio-player';
 import Audio from 'react-audioplayer';
-import playlist from './playlist.json'
+import {playlist} from './playlist.json';
 
 // require('wavesurfer.js');
+const styles =  {
+       flex: 1,
+		//width: 300,
+      //  margin: auto,
+				marginTop: 20,
+	// 	display: 'flex',
+	// justifyContent: 'center'
+	// // position: relative,
+	// left:auto,
+	// right:auto;
 
-const styles = {
-    width: '100%',
-};
 
+	}
 
 class AudioPlayer extends Component {
 	constructor(props) {
     super(props);
- 
+
     this.state = {
       playing: false,
       pos: 0
@@ -36,27 +44,27 @@ class AudioPlayer extends Component {
     });
   }
  render() {
-	 
-	 
+
+
 	 let element = document.getElementById("playerContainer").style.display = 'none';
-	 
+
 	 let elementChat = document.getElementById("chat").style.display = 'none';
 
 /*	 window.onload = function() {
   document.getElementById('playerChatContainer').style.display = 'none';
 };*/
-	
+
    return (
-		 <div className= 'audioBox'>
+		 <div>
 		 <Audio
-  width={600}
-  height={400}
-	fullPlayer={true}
-	comment={true}
-	onCommentSubmit={(text) => {alert(text); }}
-  autoPlay={false}
-  playlist={playlist.playlist}
+  // width={500}
+  // height={100%}
+	// flex={2}
 	style={styles}
+	fullPlayer={true}
+	comment={false}
+  autoPlay={false}
+  playlist={playlist}
 />
       		 	 </div>
       );
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route } from 'react-router';
 
 import App from './App';
-import AudioPlayer from './pages/mixes/AudioPlayer';
+import AudioPlayerPage from './pages/mixes/AudioPlayerPage';
 import NotFound from './pages/NotFound/NotFound';
 import Live from './pages/live/live';
 
@@ -10,7 +10,7 @@ const Routes = (props) => (
  <Router {...props}>
    <Route path="/" component={App}>
 				<Route path="/live" component={Live} />
-	      <Route path="/mixes" component={AudioPlayer} />
+	      <Route path="/mixes" component={AudioPlayerPage} />
        <Route path="*" component={NotFound} />
    </Route>
  </Router>
